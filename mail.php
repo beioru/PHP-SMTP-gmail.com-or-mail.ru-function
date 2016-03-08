51a5668522b5e02d543d1942de6e939075b76c02
materialdesignru <site-maker@bk.ru>
2016-03-08 18:12:23 +0300

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,128 @@
+<?
+// part of material-design.ru please set a simple link on my site =) <a href="material-design.ru">material-design.ru</a>
+// setup config and use like mail(): mail_now('admin@site','Hello','I send email');
+// =morfiysss=
+
+mail_now('admin@site.com','Hello','I send email');
+
+function mail_now($mailo, $theme, $mail_text, $header=''){
+$smtp_mail_config['service_name']  = 'google';//mail/google
+$smtp_mail_config['smtp_username'] = "";  //username
+$smtp_mail_config['smtp_from']     = ''; //Site domen or name
+$smtp_mail_config['smtp_password'] = '';  //password
+$smtp_mail_config['smtp_charset']  = 'utf-8';  //encoding
+
+if(!($ret=smtp_mail($smtp_mail_config, $mailo, $theme, $mail_text, $header))) $ret=mail($mailo, $theme, $mail_text, $header);
+return  $ret;
+}
+function smtp_mail($smtp_mail_config, $mail_to, $subject, $message, $headers='') {
+    $smtp_mail_config['smtp_debug']   = false; //debug mode
+    $SEND = "Date: ".date("D, d M Y H:i:s") . " UT\r\n";
+    $SEND .=    'Subject: =?'.$smtp_mail_config['smtp_charset'].'?B?'.base64_encode($subject)."=?=\r\n";
+    if ($headers) $SEND .= $headers."\r\n\r\n";
+    else
+    {
+            $SEND .= $smtp_mail_config['smtp_username']?"Reply-To: ".$smtp_mail_config['smtp_username']."\r\n":'';
+            $SEND .= "MIME-Version: 1.0\r\n";
+            $SEND .= $smtp_mail_config['smtp_charset']?"Content-type: text/html; charset=\"".$smtp_mail_config['smtp_charset']."\"\r\n":'';
+            $SEND .= "Content-Transfer-Encoding: 8bit\r\n";
+            $SEND .= $smtp_mail_config['smtp_from']?"From: \"".$smtp_mail_config['smtp_from']."\" <".$smtp_mail_config['smtp_username'].">\r\n":'';
+            $SEND .= $mail_to?"To: $mail_to <$mail_to>\r\n":'';
+            $SEND .= "X-Priority: 3\r\n\r\n";
+    }
+    $SEND .=  $message."\r\n";
+    switch ($smtp_mail_config['service_name']) {
+        case 'mail':
+            $service['smtp_host']='tls://smtp.mail.ru';
+            $service['smtp_port']='465';
+            $service['hello']='EHLO mail';
+
+        break;
+        case 'google':
+            $service['smtp_host']='ssl://smtp.gmail.com';
+            $service['smtp_port']='465';
+            $service['hello']='HELO google';
+
+        break;
+        
+        default:
+            # code...
+            break;
+    }
+     if( !$socket = fsockopen($service['smtp_host'], $service['smtp_port'], $errno, $errstr,30) ) {
+        if ($smtp_mail_config['smtp_debug']) echo $errno."<br>".$errstr;
+        return false;
+     }
+ 
+    if (!server_parse($smtp_mail_config, $socket, "220", __LINE__)) return false;
+ 
+    fputs($socket, $service['hello']."\r\n");
+    if (!server_parse($smtp_mail_config, $socket, "250", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Cannot send HELO!</p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, "AUTH LOGIN\r\n");
+    if (!server_parse($smtp_mail_config, $socket, "334", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Can not auth.</p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, base64_encode($smtp_mail_config['smtp_username']) . "\r\n");
+    if (!server_parse($smtp_mail_config, $socket, "334", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Wrong login</p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, base64_encode($smtp_mail_config['smtp_password']) . "\r\n");
+    if (!server_parse($smtp_mail_config, $socket, "235", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Wrong pass</p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, "MAIL FROM: <".$smtp_mail_config['smtp_username'].">\r\n");
+    if (!server_parse($smtp_mail_config, $socket, "250", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Can not send MAIL FROM: </p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, "RCPT TO: <" . $mail_to . ">\r\n");
+ 
+    if (!server_parse($smtp_mail_config, $socket, "250", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Can not send  RCPT TO: </p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, "DATA\r\n");
+ 
+    if (!server_parse($smtp_mail_config, $socket, "354", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Can not send  DATA</p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, $SEND."\r\n.\r\n");
+ 
+    if (!server_parse($smtp_mail_config, $socket, "250", __LINE__)) {
+        if ($smtp_mail_config['smtp_debug']) echo '<p>Can not send text</p>';
+        fclose($socket);
+        return false;
+    }
+    fputs($socket, "QUIT\r\n");
+    fclose($socket);
+    return TRUE;
+}
+ 
+function server_parse($smtp_mail_config,$socket, $response, $line = __LINE__) {
+    while (@substr($server_response, 3, 1) != ' ') {
+        if (!($server_response = fgets($socket, 256))) {
+            if ($smtp_mail_config['smtp_debug']) echo "<p>Problem.</p>$response<br>$line<br>";
+            return false;
+        }
+    }
+    if (!(substr($server_response, 0, 3) == $response)) {
+        if ($smtp_mail_config['smtp_debug']) echo "<p>Problem.!</p>$response<br>$line<br>";
+        return false;
+    }
+    return true;
+}
+?>
