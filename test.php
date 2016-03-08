d0429e5baf4407fc59da8e37b5a9453c4c72d0ec
materialdesignru <site-maker@bk.ru>
2016-03-08 18:29:45 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,18 @@
 <?php
+// part of material-design.ru please set a simple link on my site =) <a href="http://material-design.ru">material-design.ru</a>
+// =morfiysss=
+//first usage
 include 'mail.php';
 
-function mail_now($mailo, $theme, $mail_text, $header=''){
-$smtp_mail_config['service_name']  = 'google'; //mail or google
-$smtp_mail_config['smtp_username'] = "";       //username
-$smtp_mail_config['smtp_from']     = '';       //Site domen or name
-$smtp_mail_config['smtp_password'] = '';       //password
-$smtp_mail_config['smtp_charset']  = 'utf-8';  //encoding
-return cmail_now($smtp_mail_config, $smtp_mail_config, $mailo, $theme, $mail_text, $header);
+function mail_now($mailto, $theme, $mail_text, $header=''){
+  $smtp_mail_config['service_name']  = 'gmail.com'; //mail.ru or gmail.com
+  $smtp_mail_config['smtp_username'] = "";       //username
+  $smtp_mail_config['smtp_from']     = '';       //Site domen or name
+  $smtp_mail_config['smtp_password'] = '';       //password
+  $smtp_mail_config['smtp_charset']  = 'utf-8';  //encoding
+  return cmail_now($smtp_mail_config, $smtp_mail_config, $mailto, $theme, $mail_text, $header);
 }
-
-mail_now('admin@site.com','Hello','I send email');
+//then use like mail('admin@site.com', 'Hello', 'I send email');
+mail_now('admin@site.com', 'Hello', 'I send email');
 
 ?>
